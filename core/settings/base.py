fdb0226a048f0560cdebd9cc64bcefba19b0a4e2
Flávio Juvenal <flaviojuvenal@gmail.com>
2016-02-23 16:33:48 -0300

    Logging on base

--- core/settings/base.py
+++ core/settings/base.py
@@ -122,3 +122,20 @@ REST_FRAMEWORK = {
         'bikeways.filter_backends.LimitFilterBackend',
     )
 }
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
